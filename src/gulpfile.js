41ce9a04b180f5fa59259a7e850106039c11ec53
Fernando Cueto <fernandoecueto@gmail.com>
2025-04-21 18:42:37 +0200

    Agregar archivos de configuración y optimización: incluir gulpfile.js para la gestión de tareas, añadir archivos SCSS y JS, y actualizar .gitignore para excluir archivos temporales y de compilación. También se incluyen los archivos minificados y sus mapas de origen en assets, y se actualiza theme.liquid para referenciarlos.

---
+++ src/gulpfile.js
@@ -0,0 +1,43 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+const paths = {
+  styles: {
+    src: 'scss/**/*.scss',
+    dest: '../assets/',
+  },
+  scripts: {
+    src: 'js/**/*.js',
+    dest: '../assets/',
+  },
+};
+
+function styles() {
+  return src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(paths.styles.dest));
+}
+
+function scripts() {
+  return src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(paths.scripts.dest));
+}
+
+function watchFiles() {
+  watch(paths.styles.src, styles);
+  watch(paths.scripts.src, scripts);
+}
+
+exports.default = series(styles, scripts, watchFiles);
